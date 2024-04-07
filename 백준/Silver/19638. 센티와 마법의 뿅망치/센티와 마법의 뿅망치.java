ea45e90d8a042ab4af65679e67ecf8d100e284a3
hyunjeong kim <144875940+hyunjeongeeee@users.noreply.github.com>
2024-04-07 20:03:39 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 416 ms, Memory: 29008 KB -BaekjoonHub

---
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 뿅망치에 맞은 사람의 키가 ⌊ 뿅망치에 맞은 사람의 키 / 2 ⌋로 변하는 마법 도구
+        // 단, 키가 1인 경우 더 줄어들 수가 없어 뿅망치의 영향을 받지 않는다. 횟수 제한이 있다.
+        // 매번 가장 키가 큰 거인 가운데 하나를 때리는 것
+        // 거인의 나라의 모든 거인이 센티보다 키가 작도록 할 수 있을까?
+
+        /*
+        모든거인이 센티보다 작아지면 "YES + 마법의 뿅망치를 최소로 사용한 횟수"
+        센티보다 키가 크거나 같은 거인이 있는 경우 "NO + 키가 가장 큰 거인의 키"
+
+
+        [출력 내용]
+         N (거인나라 인구수)  |  H (센티의 키)  |  T (마법의 뿅망치 횟수 제한)
+         두 번째 줄부터 N개의 줄에 각 거인의 키
+        */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 거인 인구수
+        long H = Long.parseLong(st.nextToken());   // 센티키
+        int T = Integer.parseInt(st.nextToken());   // 뿅망치 횟수제한
+        PriorityQueue<Integer> giants = new PriorityQueue<>(Collections.reverseOrder());
+
+        // 거인 키 입력
+        while (N-- > 0) {
+            giants.add(Integer.parseInt(br.readLine()));
+        }
+
+        int max = giants.peek(); // 우선순위중 가장큰값 (삭제X)
+        int count = 0;           // 최소 횟수 저장할 변수
+
+        if(max >= H) {  // 제일큰 거인의 키가 센티보다 클경우에만 작동
+
+        // 뿅망치 잔여횟수가 남아있고 + 제일큰 거인의 키가 센티보다 클경우에만 작동
+            while(T-- > 0)  {
+
+                if(max!=1){ // 거인의 키가 1이 아닐경우 (1이면 아무 영향도 받지 않는다)
+                    count++;
+                    max = giants.poll()/ 2;  // 제일 큰값을 빼서 반으로 망치질
+                    giants.add(max);    // 다시 pq에 넣어줌
+                }
+
+                if (giants.peek() < H) {
+                    break;
+                } else if(giants.peek() >= H) {
+                    max = giants.peek();
+                }
+            }
+        }
+
+        // T를 소진하고도 (센치 >= 거인) false인 경우
+        if(max < H) {
+            bw.write("YES");
+            bw.write("\n"+ count);
+        } else {
+            bw.write("NO");
+            bw.write("\n" + max);
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
