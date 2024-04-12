7d614fe9d1ced42e2ffa40e6a47d863b48fedca5
hyunjeong kim <144875940+hyunjeongeeee@users.noreply.github.com>
2024-04-12 14:24:11 +0900

    [Silver I] Title: 회의실 배정, Time: 612 ms, Memory: 45404 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,16 +1,18 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Meeting implements Comparable<Meeting> {
+    
+    static class Meeting implements Comparable<Meeting> {  // meeting 클래스 생성
         int start;
         int end;
-
-        Meeting(int start, int end) {
+        public Meeting(int start, int end) {
             this.start = start;
             this.end = end;
         }
 
-        // 회의를 종료 시간 기준으로 정렬, 종료 시간이 같다면 시작 시간 기준으로 정렬
+        // 회의를 종료 시간 기준으로 정렬 
+        // 종료 시간이 같다면 시작 시간 기준으로 정렬
         @Override
         public int compareTo(Meeting o) {
             if (this.end == o.end) {
@@ -18,33 +20,37 @@ public class Main {
             }
             return this.end - o.end;
         }
+
+
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        List<Meeting> meetings = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Meeting> meetings = new ArrayList<>();    // 순서가 있음
 
-        for (int i = 0; i < N; i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            meetings.add(new Meeting(start, end));
+        while(N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            meetings.add(new Meeting(start, end));  // 객체 생성 후 List에 추가
         }
 
-        // 회의 리스트를 종료 시간 기준으로 정렬
-        Collections.sort(meetings);
+        // 오름차순 정렬
+        meetings.sort(Comparator.naturalOrder());
 
-        int count = 0;  // 가능한 많은 회의를 카운트
-        int lastEnd = 0;  // 마지막으로 선택된 회의의 종료 시간
-
-        for (Meeting meeting : meetings) {
-            // 현재 회의의 시작 시간이 마지막으로 선택된 회의의 종료 시간 이후면 회의 선택
-            if (meeting.start >= lastEnd) {
-                lastEnd = meeting.end;
+        int count = 0;
+        int lastEnd = 0;
+        for(Meeting meet : meetings) {
+            // 현재 회의의 시작 시간이
+            // 마지막으로 선택된 회의의 종료 시간 이후면 회의 선택
+            if(meet.start >= lastEnd) {
+                lastEnd = meet.end;
                 count++;
             }
         }
-
         System.out.println(count);
     }
+
 }
+
