4020c7604b6f050faae7d5090039cb1408894037
hyunjeong kim <144875940+hyunjeongeeee@users.noreply.github.com>
2024-04-13 13:18:52 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 508 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -34,7 +34,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         fib(n);
