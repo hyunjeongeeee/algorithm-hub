949933dec743ec183f0904dd08be907a0c85510e
hyunjeong <144875940+hyunjeongeeee@users.noreply.github.com>
2024-06-27 23:07:57 +0900

    [level 0] Title: 문자열 출력하기, Time: 149.89 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        if (a.length() >= 1 && a.length() <=10000000) {
+              System.out.println(a);
+        }
+      
+    }
+}
