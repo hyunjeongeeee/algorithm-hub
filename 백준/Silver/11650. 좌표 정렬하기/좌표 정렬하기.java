5eb71f03b6e8055ae4920fb4822094a1c9b1a6ab
hyunjeong <144875940+hyunjeongeeee@users.noreply.github.com>
2024-07-07 18:15:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1284 ms, Memory: 58976 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        StringTokenizer st = null;
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+
+        // 2차원배열 오름차순 정렬하기 (람다식 표현)
+        Arrays.sort(arr, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+        for(int[] i: arr) {
+            System.out.println(i[0] + " " + i[1]);
+        }
+    }
+}
