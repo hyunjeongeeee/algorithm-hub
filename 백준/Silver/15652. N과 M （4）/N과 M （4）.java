6a8d2974f0bf522bd9e52bf14b810d8f0e59144e
hyunjeong <hyunjeong9592@gmail.com>
2024-07-30 10:11:41 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 19900 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[] visited;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        selected = new int[N];
+        dfs(N, M,0, 1);
+        System.out.println(sb);
+    }
+
+    static void dfs(int n, int m, int depth, int start) {
+        if(depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(selected[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i <= n; i++) {
+                selected[depth] = i;
+                dfs(n, m,depth + 1, i);
+        }
+    }
+}
