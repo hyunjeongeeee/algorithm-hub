a8eaa5d15d281fd4e18149e0d54e02b9db0e2c27
hyunjeong <hyunjeong9592@gmail.com>
2024-08-17 23:59:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ch = br.readLine().charAt(0);
+        System.out.println(ch);
+    }
+}
