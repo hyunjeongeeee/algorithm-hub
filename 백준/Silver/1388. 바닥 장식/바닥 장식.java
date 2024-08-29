dfbc1191ae4172f5120e9b8557dfaca8db550c53
hyunjeong <hyunjeong9592@gmail.com>
2024-08-29 21:07:22 +0900

    [Silver IV] Title: 바닥 장식, Time: 100 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -6,6 +6,8 @@ public class Main {
     static int N, M;
     static char[][] floor;
     static boolean[][] visited;
+    static int[] dx; // 방향을 결정하는 배열
+    static int[] dy;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,40 +16,29 @@ public class Main {
         M = Integer.parseInt(input[1]);
         floor = new char[N][M];
         visited = new boolean[N][M];
-
+        
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
             for (int j = 0; j < M; j++) {
                 floor[i][j] = line.charAt(j);
             }
         }
-
+        
         int count = 0;
 
-        // 가로 방향 탐색
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (floor[i][j] == '-' && !visited[i][j]) {
-                    count++;
-                    // 가로로 연속된 '-'를 모두 방문 처리
-                    while (j < M && floor[i][j] == '-') {
-                        visited[i][j] = true;
-                        j++;
+                if (!visited[i][j]) {
+                    // 탐색할 방향 결정
+                    if (floor[i][j] == '-') {
+                        dx = new int[]{0}; // 오른쪽으로만 이동
+                        dy = new int[]{1};
+                    } else if (floor[i][j] == '|') {
+                        dx = new int[]{1}; // 아래쪽으로만 이동
+                        dy = new int[]{0};
                     }
-                }
-            }
-        }
-
-        // 세로 방향 탐색
-        for (int j = 0; j < M; j++) {
-            for (int i = 0; i < N; i++) {
-                if (floor[i][j] == '|' && !visited[i][j]) {
+                    dfs(i, j, floor[i][j]);
                     count++;
-                    // 세로로 연속된 '|'를 모두 방문 처리
-                    while (i < N && floor[i][j] == '|') {
-                        visited[i][j] = true;
-                        i++;
-                    }
                 }
             }
         }
@@ -55,22 +46,16 @@ public class Main {
         System.out.println(count);
     }
 
-    /*
-     '-' 는 가로 인덱스랑 같은지 비교 => x값 고정
-    0 0 [x][y]
-    0 1 [x][y + 1]
-    0 2 [x][y + 2]
-    0 3
-    ...
-    ----------------------------
-     '|' 는 세로 인덱스랑 같은지 비교 => y값 고정
-    0 0 [x][y]
-    1 0 [x + 1][y]
-    2 0 [x + 2][y]
-    3 0
-    ...
+    static void dfs(int x, int y, char type) {
+        visited[x][y] = true;
 
-    같은 무늬일 경우 [인덱스 +1] 반복해서 다음 요소와 같은지 다른지 확인 (다른 무늬가 나올때 까지 반복 탐색)
-    다른경우 반복문 stop => 타일갯수 count + 1 => 다음 인덱스 탐색
-    */
+        for (int i = 0; i < dx.length; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx < N && ny < M && !visited[nx][ny] && floor[nx][ny] == type) {
+                dfs(nx, ny, type);
+            }
+        }
+    }
 }
