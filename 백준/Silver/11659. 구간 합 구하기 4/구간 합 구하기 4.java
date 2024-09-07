d2791a648d8abae84b49d2198b27a83656b86b28
hyunjeong <hyunjeong9592@gmail.com>
2024-09-07 21:38:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1096 ms, Memory: 59940 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1]; // 1-based index로 처리하기 위해 배열 크기를 N+1로 설정
+        int[] prefixSum = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i - 1] + arr[i]; // 누적 합 배열 계산
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
+            int sum = prefixSum[second] - prefixSum[first - 1]; // 구간 합 계산
+            System.out.println(sum);
+        }
+    }
+}
