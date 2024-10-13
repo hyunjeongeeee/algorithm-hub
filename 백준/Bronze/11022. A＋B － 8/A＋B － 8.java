ba8d0023c765f477e15b84309564c58b6ed8dff8
hyunjeong <hyunjeong9592@gmail.com>
2024-10-13 09:43:30 +0900

    [Bronze V] Title: A+B - 8, Time: 148 ms, Memory: 16340 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        for(int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a + b;
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + sum);
+        }
+    }
+}
