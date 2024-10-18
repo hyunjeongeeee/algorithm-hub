c329aa4f7d33d3d5b5a14f02951920530619707c
hyunjeong <hyunjeong9592@gmail.com>
2024-10-18 22:38:32 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 748 ms, Memory: 236040 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int test = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        while(test-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a + b + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
