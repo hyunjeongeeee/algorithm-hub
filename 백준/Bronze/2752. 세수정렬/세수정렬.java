5b64a7377953f1bf37a819b6693d33c09a47b6f0
hyunjeong <hyunjeong9592@gmail.com>
2024-11-06 23:28:48 +0900

    [Bronze IV] Title: 세수정렬, Time: 120 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }  
+}
